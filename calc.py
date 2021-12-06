e7c88661ad2769df944098e8b9ea586e9a9ebc86
sasidharsub <95578948+sasidharsub@users.noreply.github.com>
2021-12-06 18:31:58 +0530

    wrong codes corrected

--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
-print('Nidharshu')
-print('Welcome')
-print('Take rest')
+class Calc():
+  def add(a,b):
+    return a+b
+  def sub(a,b):
+    return a-b
+  def mul(a,b):
+    return a*b
+  def div(a,b):
+    return a/b
